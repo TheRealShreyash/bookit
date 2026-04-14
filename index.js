59dcd71e8a7f7050b1daf838b0ee130405913aa1
TheRealShreyash <koshta.shreyash02@gmail.com>
2026-04-14 15:47:17 +0530

    connect hosted db

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ const port = process.env.PORT || 8080;
 // If you pick one connection out of the pool and release it
 // the pooler will keep that connection open for sometime to other clients to reuse
 const pool = new pg.Pool({
-  host: "localhost",
-  port: 5432,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: "postgres",
-  password: "postgres",
-  database: "bookit",
+  password: process.env.DB_PASSWORD,
+  database: "railway",
   max: 20,
   connectionTimeoutMillis: 0,
   idleTimeoutMillis: 0,
