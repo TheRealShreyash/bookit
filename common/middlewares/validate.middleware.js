e1b8080ea55ef6b2aa4c8234d57e808edae0766a
TheRealShreyash <koshta.shreyash02@gmail.com>
2026-04-14 22:30:23 +0530

    fix: handle response at dto layer

--- common/middlewares/validate.middleware.js
+++ common/middlewares/validate.middleware.js
@@ -1,16 +1,21 @@
 import ApiError from "../utils/api-error.js";
+import ApiResponse from "../utils/api-response.js";
 
 const validate = (schema) => {
-  return (req, _, next) => {
-    const result = schema.safeParse(req.body);
+  return (req, res, next) => {
+    try {
+      const result = schema.safeParse(req.body);
 
-    if (!result.success) {
-      const errors = result.error.issues.map((e) => e.message).join("; ");
-      throw ApiError.badRequest(errors);
+      if (!result.success) {
+        const errors = result.error.issues.map((e) => e.message).join("; ");
+        throw ApiError.badRequest(errors);
+      }
+
+      req.body = result.data;
+      next();
+    } catch (error) {
+      ApiResponse.error(res, error);
     }
-
-    req.body = result.data;
-    next();
   };
 };
 
