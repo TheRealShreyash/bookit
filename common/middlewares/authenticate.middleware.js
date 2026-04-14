e60eed34e7bb84cd8aa37bf144ccff83479732b6
TheRealShreyash <koshta.shreyash02@gmail.com>
2026-04-14 09:13:58 +0530

    add: login page & protect routes

--- common/middlewares/authenticate.middleware.js
+++ common/middlewares/authenticate.middleware.js
@@ -4,40 +4,56 @@ import ApiResponse from "../utils/api-response.js";
 
 export const authenticate = () => {
   return (req, res, next) => {
-    const header = req.headers["authorization"];
+    try {
+      const header = req.headers["authorization"];
 
-    if (!header) return next();
+      if (!header) return next();
 
-    if (!header.startsWith("Bearer")) {
-      return ApiResponse.error(
+      if (!header.startsWith("Bearer")) {
+        return ApiResponse.error(
+          res,
+          ApiError.badRequest("Authorization header must start with Bearer"),
+        );
+      }
+
+      const token = header.split(" ")[1];
+
+      if (!token)
+        return ApiResponse.error(
+          res,
+          ApiError.badRequest("Token missing from authorization header"),
+        );
+
+      const user = verifyUserAccessToken(token);
+
+      req.user = user;
+
+      next();
+    } catch (error) {
+      ApiResponse.error(
         res,
-        ApiError.badRequest("Authorization header must start with Bearer"),
+        ApiError.unauthorized("Session expired or invalid token"),
       );
     }
-
-    const token = header.split(" ")[1];
-
-    if (!token)
-      return ApiResponse.error(
-        res,
-        ApiError.badRequest("Token missing from authorization header"),
-      );
-
-    const user = verifyUserAccessToken(token);
-
-    req.user = user;
-
-    next();
   };
 };
 
 export const restrictToAuthenticatedUser = () => {
   return (req, res, next) => {
-    if (!req.user)
-      return res.status(401).json({
-        success: false,
-        message: "Authentication required",
-      });
-    next();
+    try {
+      if (!req.user) {
+        const acceptsHtml = req.headers["accept"]?.includes("text/html");
+        if (acceptsHtml) {
+          return res.redirect("/login");
+        }
+        return res.status(401).json({
+          success: false,
+          message: "Authentication required",
+        });
+      }
+      next();
+    } catch (error) {
+      ApiResponse.error(res, ApiError.badRequest("User not logged in"));
+    }
   };
 };
